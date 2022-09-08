9b838bb9300e02b432f60746e00407aae2c02d78
thinhnc <thinhnc@hiworld.com.vn>
2022-09-08 14:39:26 +0700

    mintNFT

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from web3 import Web3
 import json
 import requests
+import config
 
 app = FastAPI()
 bsc_test = "https://data-seed-prebsc-1-s1.binance.org:8545/"
 bsc = "https://bsc-dataseed.binance.org/"
 
+
+
 @app.get("/") 
 async def Welcome():
     return {"Hello World"}
+
 
 @app.get("/balance")
 async def Balance(address):
@@ -39,7 +43,8 @@ async def Tokens(token_address, my_address):
 @app.get("/getPrice/{A}/{B}")
 async def getPrice(A,B):
     web3 = Web3(Web3.HTTPProvider(bsc))
-    pancake_factory_address = web3.toChecksumAddress("0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73")
+    pancake = web3.toChecksumAddress(config.PANCAKE_FACTORY_ADDRESS)
+    pancake_factory_address = web3.toChecksumAddress(pancake)
     TK_A = web3.toChecksumAddress(A)
     TK_B = web3.toChecksumAddress(B)
     InputTokenAddr = TK_A
@@ -58,3 +63,26 @@ async def getPrice(A,B):
     reserve0 = reserves[0]
     reserve1 = reserves[1]
     return {"Pair A/B":reserve0/reserve1, "Pair B/A":reserve1/reserve0}
+
+@app.get("/mintNFT/{receiver}/{type}/{rarity}")
+async def mint(receiver, type:int, rarity:int):
+    web3 = Web3(Web3.HTTPProvider(bsc_test))
+    wallet = web3.toChecksumAddress(config.WALLET_ADDRESS)
+    nft_address = web3.toChecksumAddress(config.NFT_ADDRESS)
+    key = config.PRIVATE_KEY
+    url_eth = "https://api-testnet.bscscan.com/api"
+    API_ENDPOINT = url_eth+"?module=contract&action=getabi&address="+str(nft_address)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    r = requests.get(url = API_ENDPOINT, headers=headers)
+    response = r.json()
+    abi=json.loads(response["result"])
+    contractNFT = web3.eth.contract(address=nft_address, abi=abi)
+
+    mint = contractNFT.functions.mintNFT(receiver, type, rarity).buildTransaction({
+        'from': wallet,
+        'gasPrice': web3.toWei('10', 'gwei'),
+        'nonce': web3.eth.get_transaction_count(wallet)
+    })
+    signed_txn = web3.eth.account.sign_transaction(mint, private_key=key)
+    tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    return {"Minted": web3.toHex(tx_token)}
